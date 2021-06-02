6920d1bd4e8abb963b4373b1fded42f964da6274
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-02 12:29:38 +0000

    [終了]Bookers2のいいねとコメントを非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.comments.new(comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     Comment.find_by(book_id: params[:book_id], id: params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
   end
 
     private
